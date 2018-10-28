60eb7c9d7c33865d9997d441c38834e656afe2b4
Stephanie Wu <stephwu2000@gmail.com>
2018-10-27 18:59:35 -0700

    Revert previous commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
     <Router>
