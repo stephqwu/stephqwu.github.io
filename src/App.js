fe6920648fc9729e496b1974064091ff5c2242ec
Stephanie Wu <stephanie.wu@later.com>
2019-02-06 23:02:10 -0800

    Display social icons

--- src/App.js
+++ src/App.js
@@ -217,19 +217,19 @@ Stephanie (italics) is a 3rd year student at the university of british columbia 
           </footer>*/} 
 <br></br>
 <br></br>
-<Footer color="indigo" className="font-small pt-0">
+<Footer className="font-small pt-0">
                 <Container>
                     <Row>
-                        <Col md="12">
-                            <div className="mb-5 flex-center">
-                            <a className="fb-ic"><i className="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
-                            <a className="tw-ic"><i className="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
-                            <a className="gplus-ic"><i className="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
-                            <a className="li-ic"><i className="fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
-                            <a className="ins-ic"><i className="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
-                            <a className="pin-ic"><i className="fa fa-pinterest fa-lg white-text fa-2x"> </i></a>
+                        <div class="col-md-12 py-5">
+                            <div class="mb-5 flex-center">
+                            <a className="fb-ic"><i className="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x" style={{color:"#D3D3D3", padding:12}}> </i></a>
+                            <a className="tw-ic"><i className="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x" style={{color:"#D3D3D3", padding:12}}> </i></a>
+                            <a className="gplus-ic"><i className="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x" style={{color:"#D3D3D3", padding:12}}> </i></a>
+                            <a className="li-ic"><i className="fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x" style={{color:"#D3D3D3", padding:12}}> </i></a>
+                            <a className="ins-ic"><i className="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x" style={{color:"#D3D3D3", padding:12}}> </i></a>
+                            <a className="pin-ic"><i className="fa fa-pinterest fa-lg white-text fa-2x" style={{color:"#D3D3D3", padding:12}}> </i></a>
                             </div>
-                        </Col>
+                        </div>
                     </Row>
                 </Container>
 <br></br>
